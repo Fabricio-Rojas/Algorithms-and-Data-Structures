4c691ff1ca4601098c1cb2a4872ab541381fefe1
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-05-31 09:05:04 -0500

    defining my functions

--- Intro to .Net and C#/Program.cs
+++ Intro to .Net and C#/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Text;
+
+char[] getRepeatChars()
+{
+    char[] charArray = new char[5];
+    return charArray;
+}
+
+string[] getUniqueWords()
+{
+    string[] stringArray = new string[5];
+    return stringArray;
+}
+
+string reverseString()
+{
+    return "reversed string";
+}
+
+string getLongestWord()
+{
+    return "Longest word";
+}
+
+StringBuilder stringBuilder = new StringBuilder();
+// check constructor definition
