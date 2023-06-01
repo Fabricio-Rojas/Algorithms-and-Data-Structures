0df51c3813ae36c809c77a78918c6eb7fe8e2fd5
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-05-31 23:04:55 -0500

    finished lab

--- Intro to .Net and C#/Program.cs
+++ Intro to .Net and C#/Program.cs
@@ -1,43 +1,93 @@
 ﻿using System.Text;
 
-char[] getRepeatChars(string stringToCheck)
+void getRepeatChars(string stringToCheck)
 {
-    char[] charArray;
+    StringBuilder sb = new StringBuilder();
+
+    sb.Append('[');
+
     for (int i = 0; i < stringToCheck.Length; i++)
     {
         if (!(stringToCheck.IndexOf(stringToCheck[i]) == i))
         {
-            charArray.Append(stringToCheck[i]);
+            sb.Append(Char.ToLower(stringToCheck[i]));
+            sb.Append(", ");
         }
     }
-    return charArray;
+
+    sb.Append("]");
+    Console.WriteLine("The repeat characters in '{0}' are:", stringToCheck);
+    Console.WriteLine(sb.ToString());
 }
 
-getRepeatChars("The World Revolving!");
+getRepeatChars("Programmatic Python");
+Console.WriteLine();
 
-string[] getUniqueWords()
+void getUniqueWords(string sentence)
 {
-    string[] stringArray = new string[5];
-    return stringArray;
+    StringBuilder sb = new StringBuilder();
+
+    sb.Append('[');
+
+    string[] strArray = sentence.ToLower().Split(' ');
+
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        if (Array.IndexOf(strArray, strArray[i]) == i)
+        {
+            sb.Append(strArray[i]);
+            sb.Append(", ");
+        }
+    }
+
+    sb.Append(']');
+    Console.WriteLine("The unique charactes in the sentence '{0}' are:", sentence);
+    Console.WriteLine(sb.ToString());
 }
 
-string reverseString()
+getUniqueWords("To be or not to be");
+Console.WriteLine();
+
+void reverseString(string strToBeReversed)
 {
-    return "reversed string";
+    char[] reversedString = new char[strToBeReversed.Length];
+    for (int i = 0; i < strToBeReversed.Length; i++)
+    {
+        reversedString[i] = strToBeReversed[strToBeReversed.Length - i - 1];
+    }
+    Console.WriteLine("The reverse version of '{0}' is:", strToBeReversed);
+    Console.WriteLine(reversedString);
 }
 
-string getLongestWord()
+reverseString("reverse");
+Console.WriteLine();
+
+void getLongestWord(string sentence)
 {
-    return "Longest word";
+    string[] strArray = sentence.Split(" ");
+
+    string longestWord = "";
+
+    for (int i = 0;i < strArray.Length;i++)
+    {
+        if (strArray[i].Length >= longestWord.Length)
+        {
+            longestWord = strArray[i];
+        }
+    }
+    Console.WriteLine("The longest word in the sentence '{0}' is:", sentence);
+    Console.WriteLine(longestWord);
 }
 
-StringBuilder strBldr = new StringBuilder();
-// check constructor definition
+getLongestWord("Tiptoe through the tulips");
 
-strBldr.Append("Hello");
-strBldr.Append(" ");
-strBldr.Append("World");
-strBldr.Append("!");
-
-strBldr.Replace(" ", null, 0, strBldr.Length);
-
+/*
+ * The main benefit of the StringBuilder class over the String class is that the data type stored within this class is mutable, meaning
+ * that the value of the string can be modified without needing to create copies and replace references, this leads to better performance
+ * when managing big volumes of string data, this is further boosted with the provided methods 'append, insert, replace and remove'.
+ * 
+ * The main disadvantage is that StringBuilder does not throw any errors at compile-time, meaning that any miscalculation in the manipulation
+ * of the string will only ever be realized in run-time, leading to unexpected errors.
+ * The second disadvantage that StringBuilder suffers from is from the inaccesibility of some methods of String, such as 'substring, toUpper, 
+ * toLower and split' these can only be accesed after turning the StringBuilder back into a String with .toString()
+*/
